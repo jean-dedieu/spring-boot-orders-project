d72d5a86c57b506937c7d9bd202d090ab77a2dd4
jean-dedieu <jeandedieu.twagirumuhoza@gmail.com>
2022-04-10 14:48:58 +0200

    implemented OrdersController

---
+++ src/main/java/com/cap/shop/controllers/OrdersController.java
@@ -0,0 +1,33 @@
+package com.cap.shop.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cap.shop.dao.OrdersRepository;
+import com.cap.shop.entities.Orders;
+
+@Controller
+public class OrdersController {
+	
+	@Autowired
+	private OrdersRepository orderRepository;
+	
+	@RequestMapping(value = "/adminorders")
+	 
+		public String displayOrdersInAdminDashBoard(Model model) {
+		
+		List<Orders> listOrd = orderRepository.listOrders(1,20);
+		model.addAttribute("listOrders", listOrd);
+		
+		return "adminordersdashboard";
+		
+	}
+	
+	
+
+}
